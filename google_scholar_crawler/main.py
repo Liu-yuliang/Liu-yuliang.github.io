0c6beefa1033dceb7bd1489ea68fb31c1751cf47
Yuliang <Liu-yuliang>
2025-06-23 16:27:52 +0800

    update main debug

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -3,12 +3,17 @@ import jsonpickle
 import json
 from datetime import datetime
 import os
-
+print(1)
 author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+print(2)
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+print(3)
 name = author['name']
+print(4)
 author['updated'] = str(datetime.now())
+print(5)
 author['publications'] = {v['author_pub_id']:v for v in author['publications']}
+print(6)
 print(json.dumps(author, indent=2))
 os.makedirs('results', exist_ok=True)
 with open(f'results/gs_data.json', 'w') as outfile:
